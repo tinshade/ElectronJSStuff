90f59ee441459f058abf2f481b842a417ecf3a36
Abhishek Iyengar <tinshade98@gmail.com>
2019-11-10 20:51:04 +0530

    Minor Changes

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function createWindow () {
   })
   
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
   mainWindow.webContents.on('new-window', function(e, url) {
   e.preventDefault();
   require('electron').shell.openExternal(url);
